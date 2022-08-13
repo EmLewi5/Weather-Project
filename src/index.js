a2fcb6a955e687c89b58804880d3d77293c042e4
EmLewi5 <110646955+EmLewi5@users.noreply.github.com>
2022-08-12 21:14:18 -0400

    adding functions

--- src/index.js
+++ src/index.js
@@ -38,14 +38,26 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", 
-  `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "771105fc4505ec746ba7776a63fa4590";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
+function search(city) {
+  let apiKey = "771105fc4505ec746ba7776a63fa4590";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
