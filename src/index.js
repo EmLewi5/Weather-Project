08e11287671c5c438c51decf38ea355231285bd9
EmLewi5 <110646955+EmLewi5@users.noreply.github.com>
2022-08-19 12:35:56 -0400

    removed HTML and added JS

--- src/index.js
+++ src/index.js
@@ -23,6 +23,58 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML + 
+    `
+    <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img 
+            src="https://openweathermap.org/img/wn/04d@2x.png"
+            alt=""
+            width="42"
+            />
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max">
+                18°</span>
+              <span class="weather-forecast-temperature-min">
+                12°</span>
+            </div>
+            </div>
+        `;
+  });
+
+      forecastHTML =
+  forecastHTML +
+  `
+          <div class="col-2">
+            <div class="weather-forecast-date">
+            Thu
+          </div>
+            <img 
+            src="https://openweathermap.org/img/wn/04d@2x.png"
+            alt=""
+            width="42"
+            />
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max">
+                18°</span>
+              <span class="weather-forecast-temperature-min">
+                12°</span>
+            </div>
+            </div>
+          </div>
+        </div>
+        `;
+
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -77,6 +129,8 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
